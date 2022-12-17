0f310aff35cd514448eb0aca95b051ab76c78c42
okutyann <h19981029@gmail.com>
2022-12-17 21:20:59 +0900

    ✨ オブジェクトに型をつける方法

--- index.ts
+++ index.ts
@@ -5,3 +5,29 @@ let negative: number = -0.12;
 let string: string = "ハロー";
 let double: string = "ハロー。";
 let back: string = `ハロー！`;
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "ジャック",
+  age: 21,
+};
+console.log(person.age);
+
+//ネストも可能
+const person1 = {
+  name: {
+    first: "ジャック",
+    last: "Smith",
+  },
+  age: 21,
+};
+
+//この書き方もありだが、すごく一般的なオブジェクトな型のため使用しない
+const person2: object = {
+  name: "ジャック",
+  age: 21,
+};
+//エラーでる
+console.log(person2.age);
